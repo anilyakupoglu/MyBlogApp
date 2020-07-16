bd1eb79f89220d0e6b80eba8e971185199facb84
Anıl Yakupoğlu <52348114+anilyakupoglu@users.noreply.github.com>
2020-07-16 13:47:34 +0300

    Admin dashboard edited

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyBlogApp.Areas.Admin.ViewModel;
 using MyBlogApp.Models;
 
 namespace MyBlogApp.Areas.Admin.Controllers
@@ -10,15 +11,39 @@ namespace MyBlogApp.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        MyBlogContext db = new MyBlogContext(); 
- 
+        MyBlogContext db = new MyBlogContext();
+
         public ActionResult Index()
         {
-            ViewBag.BlogCount = db.Blogs.Count();
-            ViewBag.ActiveBlog = db.Blogs.Where(a => a.IsActive).Count();
-            ViewBag.ActiveHome = db.Blogs.Where(a => a.HomePage).Count();
-            ViewBag.PasiveBlog = db.Blogs.Where(a => a.HomePage == false).Count();
-            return View(db.Blogs.ToList());
+            ViewBag.BlogCount = BlogCount();
+            ViewBag.ActiveBlog = IsActiveCount();
+            ViewBag.ActiveHome = ActiveHomepageCount();
+            ViewBag.PasiveBlog = DeActiveHomepageCount();
+
+            BlogDetailViewModel blogDetailView = new BlogDetailViewModel();
+            List<Blog> blogs = db.Blogs.ToList();
+            List<Category> categories = db.Categories.ToList();
+            blogDetailView.Blogs = blogs;
+            blogDetailView.Categories = categories;
+
+            return View(blogDetailView);
+        }
+
+        public int DeActiveHomepageCount()
+        {
+            return db.Blogs.Where(a => a.HomePage == false).Count();
+        }
+        public int ActiveHomepageCount()
+        {
+            return db.Blogs.Where(a => a.HomePage).Count();
+        }
+        public int BlogCount()
+        {
+            return db.Blogs.Count();
+        }
+        public int IsActiveCount()
+        {
+            return db.Blogs.Where(a => a.IsActive).Count();
         }
     }
 }
---
+++ Areas/Admin/ViewModel/BlogDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyBlogApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBlogApp.Areas.Admin.ViewModel
+{
+    public class BlogDetailViewModel
+    {
+        public List<Blog> Blogs { get; set; }
+        public List<Category> Categories { get; set; }
+
+    }
+}
