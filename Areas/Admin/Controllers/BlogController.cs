e6650383f13693e3f3e1f73e6ff61638d396cb9c
Anıl Yakupoğlu <52348114+anilyakupoglu@users.noreply.github.com>
2020-07-18 20:52:49 +0300

    Text editor added and editor post form complated

--- Areas/Admin/Controllers/BlogController.cs
+++ Areas/Admin/Controllers/BlogController.cs
@@ -2,13 +2,16 @@
 using MyBlogApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MyBlogApp.Areas.Admin.Controllers
 {
-    [AllowAnonymous]
+    [Authorize(Roles = "Admin")]
     public class BlogController : Controller
     {
         MyBlogContext db = new MyBlogContext();
@@ -16,6 +19,63 @@ namespace MyBlogApp.Areas.Admin.Controllers
         {
             return View(db.Blogs.ToList());
         }
+
+        [HttpGet]
+        public ActionResult CreateBlog()
+        {
+            BindCategoryDDL();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateBlog(Blog blog, HttpPostedFileBase file)
+        {
+            Blog blogName = db.Blogs.FirstOrDefault(a => a.Head == blog.Head);
+
+            //TODO: jpeg olan resimleri de eklet. detayda gösterirken veri tabnından sadece adını çek
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/images"), pic);
+                file.SaveAs(path);
+                blog.Picture = pic;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+            }
+            try
+            {
+                if (blog != null && blogName == null)
+                {
+                    db.Blogs.Add(blog);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View();
+        }
+
+        private void BindCategoryDDL()
+        {
+
+            List<SelectListItem> selectListItem = new List<SelectListItem>();
+            foreach (Category item in db.Categories)
+            {
+                selectListItem.Add(new SelectListItem()
+                {
+                    Text = item.CategoryName,
+                    Value = item.CategoryId.ToString()
+                });
+            }
+            ViewBag.CategoryBind = selectListItem;
+        }
+
         public PartialViewResult CategoryList()
         {
             var categories = db.Categories.Select(a => new CategoryViewM
--- Models/Blog.cs
+++ Models/Blog.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.UI.WebControls.WebParts;
 
 namespace MyBlogApp.Models
 {
     public class Blog
     {
+        public Blog()
+        {
+            CreateDate = DateTime.Now;
+        }
         public int BlogId { get; set; }
         public string Head { get; set; }
         public string Description { get; set; }
+        [AllowHtml]
         public string Content { get; set; }
         public string Picture { get; set; }
         public DateTime CreateDate { get; set; }
